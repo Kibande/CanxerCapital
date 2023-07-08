062e9d34311e0f0ac3ff534c718bde1b47fb9c94
Kibande Steven <kibandesteven@gmail.com>
2023-07-08 11:21:14 +0300

    desparate

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import { ThemeProvider } from "styled-components";
-import { lightTheme } from "./Themes/Theme";
-import GlobalStyle from "./globalStyles";
-import Main from "./pages/Main";
-import { AnimatePresence } from "framer-motion";
-import { useMemo } from "react";
-import { useMoralis } from "react-moralis";
+import React from "react";
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  const { isWeb3Enabled, enableWeb3, isAuthenticated,isInitializing,isAuthenticating, isWeb3EnableLoading,chainId,network } =
-  useMoralis();
+import Client from './Divisions/Client/Client';
+// import Admin from './Divisions/Admin/Admin';
 
-useMemo(() => {
 
-  async function checkConnection(params) {
-    const connectorId = window.localStorage.getItem("connectorId");
-    if (isAuthenticated && !isWeb3Enabled && !isWeb3EnableLoading && !isInitializing && !isAuthenticating){
-       await enableWeb3({ provider: connectorId });
-    }else if(!isWeb3Enabled && !isWeb3EnableLoading && !isInitializing && !isAuthenticating){
-      await enableWeb3();
-    }
-  }
- try {
-  checkConnection();
- } catch (error) {
-  
- }
+const App =() => {
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-}, [isAuthenticated, isWeb3Enabled,chainId,network]);
 
-  return (
-    <>
-      <GlobalStyle />
-      <ThemeProvider theme={lightTheme}>
-        <AnimatePresence exitBeforeEnter>
-         <Main/>
-        </AnimatePresence>
-      </ThemeProvider>
-    </>
-  );
+
+    return (
+    <Routes>
+        <Route exact path='/' element={<Client />}></Route>,
+        {/* <Route exact path='/' element={<Admin />}></Route> */}
+    </Routes>
+    )
 }
 
-export default App;
+export default App
--- src/utilis/errorCodes.js
+++
@@ -1,3 +0,0 @@
-export const  errorCode_0 ="Error Code: 0";
-// something went wrong but details can be found in the console
-
